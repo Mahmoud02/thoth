98e692a5a0fa110b83d7b0ec83696e8fc88f1eee
mahmoud <mahmoudreda027@gmail.com>
2025-03-09 01:20:03 +0200

    feat: add updateBucket method to MetadataService for bucket updates

--- src/main/java/com/mahmoud/thoth/service/MetadataService.java
+++ src/main/java/com/mahmoud/thoth/service/MetadataService.java
@@ -3,6 +3,7 @@ package com.mahmoud.thoth.service;
 import java.util.Map;
 
 import com.mahmoud.thoth.model.BucketMetadata;
+import com.mahmoud.thoth.dto.UpdateBucketDTO;
 
 public interface MetadataService {
     void createBucket(String bucketName);
@@ -11,4 +12,5 @@ public interface MetadataService {
     long getBucketSize(String bucketName);
     Map<String, Object> getObjectMetadata(String bucketName, String objectName);
     Map<String, BucketMetadata> getBuckets();
+    void updateBucket(String bucketName, UpdateBucketDTO updateBucketDTO);
 }
