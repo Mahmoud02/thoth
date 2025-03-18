f4edd8b08edb87bf87860ccfddf203747a3e9820
mahmoud <mahmoudreda027@gmail.com>
2025-03-18 23:26:43 +0200

    feat: implement NamespaceRepositoryAdapter and InMemoryNamespaceStore for namespace management

---
+++ src/main/java/com/mahmoud/thoth/infrastructure/repository/NamespaceRepositoryAdapter.java
@@ -0,0 +1,63 @@
+package com.mahmoud.thoth.infrastructure.repository;
+
+import com.mahmoud.thoth.domain.port.out.NamespaceRepository;
+import com.mahmoud.thoth.namespace.model.Namespace;
+import com.mahmoud.thoth.store.NamespaceStore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class NamespaceRepositoryAdapter implements NamespaceRepository {
+
+    private final NamespaceStore namespaceStore;
+
+    @Override
+    public void createNamespace(String namespaceName) {
+        namespaceStore.createNamespace(namespaceName);
+    }
+
+    @Override
+    public void deleteNamespace(String namespaceName) {
+        namespaceStore.deleteNamespace(namespaceName);
+    }
+
+    @Override
+    public void addBucketToNamespace(String namespaceName, String bucketName) {
+        namespaceStore.addBucketToNamespace(namespaceName, bucketName);
+    }
+
+    @Override
+    public void removeBucketFromNamespace(String namespaceName, String bucketName) {
+        namespaceStore.removeBucketFromNamespace(namespaceName, bucketName);
+    }
+
+    @Override
+    public Namespace getNamespace(String namespaceName) {
+        return namespaceStore.getNamespace(namespaceName);
+    }
+
+    @Override
+    public Map<String, Namespace> getNamespaces() {
+        return namespaceStore.getNamespaces();
+    }
+
+    @Override
+    public List<Namespace> getListNamespaces() {
+        return namespaceStore.getListNamespaces();
+    }
+
+    @Override
+    public Set<String> getBucketsByNamespace(String namespaceName) {
+        return namespaceStore.getBucketsByNamespace(namespaceName);
+    }
+
+    @Override
+    public void updateNamespace(String namespaceName, String newNamespaceName) {
+        namespaceStore.updateNamespace(namespaceName, newNamespaceName);
+    }
+}
