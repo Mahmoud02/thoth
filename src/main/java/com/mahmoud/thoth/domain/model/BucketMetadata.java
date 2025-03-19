00ec588527510585516e857c29403fd8d27cf0e1
mahmoud <mahmoudreda027@gmail.com>
2025-03-19 02:56:57 +0200

    refactor: remove size field from BucketMetadata class to simplify bucket representation

--- src/main/java/com/mahmoud/thoth/domain/model/BucketMetadata.java
+++ src/main/java/com/mahmoud/thoth/domain/model/BucketMetadata.java
@@ -9,14 +9,12 @@ public class BucketMetadata {
     private String namespaceName;
     private LocalDateTime creationDate;
     private LocalDateTime lastModifiedDate;
-    private long size;
 
     public BucketMetadata(String bucketName, String namespaceName) {
         this.bucketName = bucketName;
         this.namespaceName = namespaceName;
         this.creationDate = LocalDateTime.now();
         this.lastModifiedDate = LocalDateTime.now();
-        this.size = 0;
     }
 
     public BucketMetadata() {
